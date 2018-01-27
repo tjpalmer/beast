42cf9a0268d97332b45805cd470f54daa5a00bb4
Tom <tjpalmer@tjpalmer.com>
2018-01-27 12:09:57 -0700

    Get sdl version

--- build.zig
+++ build.zig
@@ -3,5 +3,9 @@ use @import("std").build;
 pub fn build(builder: &Builder) %void {
   const exe = builder.addExecutable("beast", "src/main.zig");
   exe.setBuildMode(builder.standardReleaseOptions());
+  exe.linkSystemLibrary("SDL2.lib");
   builder.default_step.dependOn(&exe.step);
+  builder.addCIncludePath("import/sdl2/include");
+  builder.addLibPath("import/sdl2/lib/x64");
+  // try builder.copyFile("import/sdl2/lib/x64/SDL2.dll", "zig-cache");
 }
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,22 @@
-use @import("std").io;
+const std = @import("std");
+use std.debug;
+// use @import("std").fmt;
+// use @import("std").io;
+const c = @cImport({
+  @cInclude("wchar.h");
+  @cInclude("SDL.h");
+});
 
 pub fn main() %void {
-    var out = try getStdOut();
-    try out.write("Hello, world!\n");
+  // var out = try getStdOut();
+  // try out.write("Hello, world!!\n");
+  // const _ = c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO);
+  var sdl_version = c.SDL_version {.major = 0, .minor = 0, .patch = 0};
+  c.SDL_GetVersion(&sdl_version);
+  // try format(out, out.write, "");
+  // try out.write(version.major);
+  warn(
+    "sdl_version {}.{}.{}\n",
+    sdl_version.major, sdl_version.minor, sdl_version.patch
+  );
 }
