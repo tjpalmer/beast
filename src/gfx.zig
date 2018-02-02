ac7f301ab40293bc99637ee31b1e1dfe478ebd41
Tom <tjpalmer@tjpalmer.com>
2018-02-02 06:02:51 -0700

    Indent by 4 even if I don't like it

--- src/gfx.zig
+++ src/gfx.zig
@@ -4,8 +4,8 @@ use std.debug;
 use std.heap;
 use @import("./ui.zig");
 const c = @cImport({
-  @cDefine("GL_GLEXT_PROTOTYPES", "");
-  @cInclude("GLES3/gl3.h");
+    @cDefine("GL_GLEXT_PROTOTYPES", "");
+    @cInclude("GLES3/gl3.h");
 });
 
 error CompileShader;
@@ -15,124 +15,125 @@ error LinkProgram;
 
 pub const Program = struct {
 
-  program: c.GLuint,
+    program: c.GLuint,
 
-  pub fn init() %Program {
-    var program = c.glCreateProgram();
-    if (program == 0) return error.CreateProgram;
-    return Program {.program = program};
-  }
-
-  fn deinit(self: &const Program) void {
-    c.glDeleteProgram(self.program);
-  }
-
-  fn apply(self: &const Program) void {
-    c.glUseProgram(self.program);
-  }
-
-  fn attach(self: &Program, shader: &const Shader) void {
-    c.glAttachShader(self.program, shader.shader);
-  }
-
-  fn bindAttrib(
-    self: &const Program, index: c.GLuint, comptime name: []const u8
-  ) void {
-    const c_name = name ++ "\x00";
-    c.glBindAttribLocation(self.program, index, &c_name[0]);
-  }
-
-  fn link(self: &Program) %void {
-    const program = self.program;
-    c.glLinkProgram(program);
-    // Check.
-    var is_linked: c.GLint = 0;
-    c.glGetProgramiv(program, c.GL_LINK_STATUS, &is_linked);
-    if (is_linked == c.GL_FALSE) {
-      var buffer = try Buffer.init(global_allocator, ""); defer buffer.deinit();
-      var length: c.GLint = 0;
-      c.glGetProgramiv(program, c.GL_INFO_LOG_LENGTH, &length);
-      // Given length includes the null character, so we don't need the extra in
-      // the buffer.
-      try buffer.resize(usize(length) - 1);
-      c.glGetProgramInfoLog(program, length, &length, buffer.ptr());
-      warn("{}: {}", length, buffer.toSlice());
-      return error.LinkProgram;
+    pub fn init() %Program {
+        var program = c.glCreateProgram();
+        if (program == 0) return error.CreateProgram;
+        return Program {.program = program};
     }
-  }
+
+    fn deinit(self: &const Program) void {
+        c.glDeleteProgram(self.program);
+    }
+
+    fn apply(self: &const Program) void {
+        c.glUseProgram(self.program);
+    }
+
+    fn attach(self: &Program, shader: &const Shader) void {
+        c.glAttachShader(self.program, shader.shader);
+    }
+
+    fn bindAttrib(
+        self: &const Program, index: c.GLuint, comptime name: []const u8
+    ) void {
+        const c_name = name ++ "\x00";
+        c.glBindAttribLocation(self.program, index, &c_name[0]);
+    }
+
+    fn link(self: &Program) %void {
+        const program = self.program;
+        c.glLinkProgram(program);
+        // Check.
+        var is_linked: c.GLint = 0;
+        c.glGetProgramiv(program, c.GL_LINK_STATUS, &is_linked);
+        if (is_linked == c.GL_FALSE) {
+            var buffer = try Buffer.init(global_allocator, "");
+            defer buffer.deinit();
+            var length: c.GLint = 0;
+            c.glGetProgramiv(program, c.GL_INFO_LOG_LENGTH, &length);
+            // Given length includes the null character, so we don't need the
+            // extra in the buffer.
+            try buffer.resize(usize(length) - 1);
+            c.glGetProgramInfoLog(program, length, &length, buffer.ptr());
+            warn("{}: {}", length, buffer.toSlice());
+            return error.LinkProgram;
+        }
+    }
 
 };
 
 pub const Scene = struct {
 
-  fragment: Shader,
+    fragment: Shader,
 
-  program: Program,
+    program: Program,
 
-  vertex: Shader,
+    vertex: Shader,
 
-  pub fn init() %Scene {
-    var scene = Scene {
-      .fragment = undefined,
-      .program = undefined,
-      .vertex = undefined,
-    };
-    puts(??c.glGetString(c.GL_VERSION));
-    // Create shaders.
-    scene.fragment =
-      try Shader.init(c.GL_FRAGMENT_SHADER, @embedFile("fragment.glsl"));
-    errdefer scene.fragment.deinit();
-    scene.vertex =
-      try Shader.init(c.GL_VERTEX_SHADER, @embedFile("vertex.glsl"));
-    errdefer scene.vertex.deinit();
-    // Create and apply program.
-    scene.program = try Program.init();
-    scene.program.bindAttrib(0, "position");
-    scene.program.attach(scene.vertex);
-    scene.program.attach(scene.fragment);
-    try scene.program.link();
-    scene.program.apply();
-    return scene;
-  }
+    pub fn init() %Scene {
+        var scene = Scene {
+            .fragment = undefined,
+            .program = undefined,
+            .vertex = undefined,
+        };
+        puts(??c.glGetString(c.GL_VERSION));
+        // Create shaders.
+        scene.fragment =
+            try Shader.init(c.GL_FRAGMENT_SHADER, @embedFile("fragment.glsl"));
+        errdefer scene.fragment.deinit();
+        scene.vertex =
+            try Shader.init(c.GL_VERTEX_SHADER, @embedFile("vertex.glsl"));
+        errdefer scene.vertex.deinit();
+        // Create and apply program.
+        scene.program = try Program.init();
+        scene.program.bindAttrib(0, "position");
+        scene.program.attach(scene.vertex);
+        scene.program.attach(scene.fragment);
+        try scene.program.link();
+        scene.program.apply();
+        return scene;
+    }
 
-  pub fn deinit(self: &const Scene) void {
-    self.vertex.deinit();
-    self.fragment.deinit();
-    self.program.deinit();
-  }
+    pub fn deinit(self: &const Scene) void {
+        self.vertex.deinit();
+        self.fragment.deinit();
+        self.program.deinit();
+    }
 
 };
 
 pub const Shader = struct {
 
-  shader: c.GLuint,
+    shader: c.GLuint,
 
-  fn init(shader_type: c_uint, source: []const u8) %Shader {
-    // Create.
-    var shader = c.glCreateShader(shader_type);
-    // Compile.
-    const sources: ?&const u8 = &source[0];
-    const sizes = c.GLint(source.len);
-    c.glShaderSource(shader, 1, &sources, &sizes);
-    c.glCompileShader(shader);
-    // Check.
-    var is_compiled: c.GLint = 0;
-    c.glGetShaderiv(shader, c.GL_COMPILE_STATUS, &is_compiled);
-    if (is_compiled == 0) return error.CompileShader;
-    // console.log(source);
-    // console.log(gl.getShaderInfoLog(shader));
-    // All done.
-    return Shader {.shader = shader};
-  }
+    fn init(shader_type: c_uint, source: []const u8) %Shader {
+        // Create.
+        var shader = c.glCreateShader(shader_type);
+        // Compile.
+        const sources: ?&const u8 = &source[0];
+        const sizes = c.GLint(source.len);
+        c.glShaderSource(shader, 1, &sources, &sizes);
+        c.glCompileShader(shader);
+        // Check.
+        var is_compiled: c.GLint = 0;
+        c.glGetShaderiv(shader, c.GL_COMPILE_STATUS, &is_compiled);
+        if (is_compiled == 0) return error.CompileShader;
+        // console.log(source);
+        // console.log(gl.getShaderInfoLog(shader));
+        // All done.
+        return Shader {.shader = shader};
+    }
 
-  fn deinit(shader: &const Shader) void {
-    c.glDeleteShader(shader.shader);
-  }
+    fn deinit(shader: &const Shader) void {
+        c.glDeleteShader(shader.shader);
+    }
 
 };
 
 pub fn paint(window: &const Window) void {
-  c.glClearColor(0, 0, 0, 1);
-  c.glClear(c.GL_COLOR_BUFFER_BIT);
-  window.swap();
+    c.glClearColor(0, 0, 0, 1);
+    c.glClear(c.GL_COLOR_BUFFER_BIT);
+    window.swap();
 }
--- src/main.zig
+++ src/main.zig
@@ -2,41 +2,41 @@ use @import("./gfx.zig");
 use @import("./ui.zig");
 use @import("std").debug;
 const c = @cImport({
-  @cInclude("wchar.h");
-  @cInclude("SDL.h");
+    @cInclude("wchar.h");
+    @cInclude("SDL.h");
 });
 
 // Main.
 
 pub fn main() %void {
-  // warn("a\n");
-  const sdl = try Sdl.init(); defer sdl.deinit();
-  // warn("b\n");
-  try initGles3();
-  // warn("c\n");
-  const window = try Window.init(); defer window.deinit();
-  const context = try Context.init(window); defer context.deinit();
-  const scene = try Scene.init(); defer scene.deinit();
-  paint(window);
-  // warn("d\n");
-  var event = c.SDL_Event {.type = 0};
-  // warn("e\n");
-  var done = false;
-  main: while (!done) {
-    while (c.SDL_PollEvent(&event) != 0) {
-      switch (c_int(event.type)) {
-        c.SDL_QUIT => {
-          done = true;
-        },
-        c.SDL_WINDOWEVENT => switch (event.window.event) {
-          c.SDL_WINDOWEVENT_RESIZED => {
-            warn("Resize\n");
-            paint(window);
-          },
-          else => {}
-        },
-        else => {}
-      }
+    // warn("a\n");
+    const sdl = try Sdl.init(); defer sdl.deinit();
+    // warn("b\n");
+    try initGles3();
+    // warn("c\n");
+    const window = try Window.init(); defer window.deinit();
+    const context = try Context.init(window); defer context.deinit();
+    const scene = try Scene.init(); defer scene.deinit();
+    paint(window);
+    // warn("d\n");
+    var event = c.SDL_Event {.type = 0};
+    // warn("e\n");
+    var done = false;
+    main: while (!done) {
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (c_int(event.type)) {
+                c.SDL_QUIT => {
+                    done = true;
+                },
+                c.SDL_WINDOWEVENT => switch (event.window.event) {
+                    c.SDL_WINDOWEVENT_RESIZED => {
+                        warn("Resize\n");
+                        paint(window);
+                    },
+                    else => {}
+                },
+                else => {}
+            }
+        }
     }
-  }
 }
